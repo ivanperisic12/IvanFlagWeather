a7e2616c4360f003435f8915fae4538fa4434a5f
Ivan Perisic <ivan.perisic.06@gmail.com>
2026-04-27 10:16:12 -0700

    Update Flagstaff_Weather.R

--- R/Flagstaff_Weather.R
+++ R/Flagstaff_Weather.R
@@ -1,9 +1,9 @@
 #' Flagstaff Pulliam Airport weather data
 #'
-#' Weather reports each day from 2015 - 2019 from the Flagstaff Pulliam Airport
+#' Weather reports each day from 1950 - 2019 from the Flagstaff Pulliam Airport
 #' This is the clean data from Pulliam Airport weather station
 #'
-#' @format A data frame with 1719 observations and 5 variables:
+#' @format A data frame with 25449 observations and 5 variables:
 #' \describe{
 #'   \item{DATE}{Date of the weather observation.}
 #'   \item{PRCP}{Daily precipitation.}
